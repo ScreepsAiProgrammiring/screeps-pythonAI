064952d9ed4a0d31dc256fbe3dbbe013f50cf244
Pohvalister <pohvalister@mail.ru>
2024-08-04 00:32:09 +0300

    initial code commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import harvester
+from role import harvester, upgrader, builder
+
 # defs is a package which claims to export all constants and some JavaScript objects, but in reality does
 #  nothing. This is useful mainly when using an editor like PyCharm, so that it 'knows' that things like Object, Creep,
 #  Game, etc. do exist.
@@ -16,34 +17,65 @@ __pragma__('noalias', 'set')
 __pragma__('noalias', 'type')
 __pragma__('noalias', 'update')
 
+MAIN_SPAWN_NAME = 'Spawn1'
 
 def main():
     """
     Main game logic loop.
     """
 
+    # Clean up dead creep memory
+    for name in Object.keys(Memory.creeps):
+        if not Game.creeps[name]:
+            del Memory.creeps[name]
+            console.log('Clearing non-existing creep memory:', name)
+
+    harvesters = _.filter(
+        Game.creeps, lambda creep: creep.memory.role == 'harvester')
+    console.log('Harvesters: ' + len(harvesters))
+
+    if len(harvesters) < 2:
+        newName = 'Harvester' + Game.time
+        console.log('Spawning new harvester: ' + newName)
+        Game.spawns[MAIN_SPAWN_NAME].spawnCreep(
+            [WORK, CARRY, MOVE],
+            newName,
+            {'memory': {'role': 'harvester'}}
+        )
+
+    if Game.spawns[MAIN_SPAWN_NAME].spawning:
+        spawningCreep = Game.creeps[Game.spawns[MAIN_SPAWN_NAME].spawning.name]
+        Game.spawns[MAIN_SPAWN_NAME].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns[MAIN_SPAWN_NAME].pos.x + 1,
+            Game.spawns[MAIN_SPAWN_NAME].pos.y,
+            {'align': 'left', 'opacity': 0.8}
+        )
+
+    # replace this with the id of your tower
+    tower = Game.getObjectById('TOWER_ID')
+
+    if tower:
+        closest_damaged_structure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            "filter": lambda structure: structure.hits < structure.hitsMax
+        })
+        if closest_damaged_structure:
+            tower.repair(closest_damaged_structure)
+
+        closest_hostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
+        if closest_hostile:
+            tower.attack(closest_hostile)
+
     # Run each creep
     for name in Object.keys(Game.creeps):
         creep = Game.creeps[name]
-        harvester.run_harvester(creep)
 
-    # Run each spawn
-    for name in Object.keys(Game.spawns):
-        spawn = Game.spawns[name]
-        if not spawn.spawning:
-            # Get the number of our creeps in the room.
-            num_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName)
-            # If there are no creeps, spawn a creep once energy is at 250 or more
-            if num_creeps < 0 and spawn.room.energyAvailable >= 250:
-                spawn.createCreep([WORK, CARRY, MOVE, MOVE])
-            # If there are less than 15 creeps but at least one, wait until all spawns and extensions are full before
-            # spawning.
-            elif num_creeps < 15 and spawn.room.energyAvailable >= spawn.room.energyCapacityAvailable:
-                # If we have more energy, spawn a bigger creep.
-                if spawn.room.energyCapacityAvailable >= 350:
-                    spawn.createCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
-                else:
-                    spawn.createCreep([WORK, CARRY, MOVE, MOVE])
+        if creep.memory.role == 'harvester':
+            harvester.run(creep)
+        if creep.memory.role == 'upgrader':
+            upgrader.run(creep)
+        if creep.memory.role == 'builder':
+            builder.run(creep)
 
 
 module.exports.loop = main
